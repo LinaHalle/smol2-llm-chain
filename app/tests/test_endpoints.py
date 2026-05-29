ec38c260ecfaf6f44aa6778af0c4b50b2e7caa31
linahalle <hallergren.lina@gmail.com>
2026-05-29 13:44:18 +0200

    Added csv upload endpoint test

--- app/tests/test_endpoints.py
+++ app/tests/test_endpoints.py
@@ -1,6 +1,9 @@
 from fastapi.testclient import TestClient
 from app.main import app
 
+# BytesIO = fejkat filobject som beter sig som en riktig fil
+from io import BytesIO
+
 client = TestClient(app)
 
 def test_health():
@@ -14,3 +17,23 @@ def test_stats_no_dataset():
 
     assert response.status_code == 404
     assert response.json()["detail"] == "No dataset uploaded yet"
+
+def test_upload_csv_succsess():
+    csv_content = b"""a,b
+1,2
+3,4
+"""
+
+    file = BytesIO(csv_content)
+
+    response = client.post(
+        "/data/upload",
+        files={"file": ("test.csv", file, "text.csv")}
+    )
+
+    assert response.status_code == 200
+    data = response.json()
+
+    assert data ["rows"] == 2
+    assert "columns" in data
+    assert "dtypes" in data

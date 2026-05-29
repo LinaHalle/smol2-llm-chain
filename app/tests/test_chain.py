f47103f80f6d2ade76ff672489fc5e3445fec0f6
linahalle <hallergren.lina@gmail.com>
2026-05-29 15:29:37 +0200

    Added unit tests for chain component LLMRunner

--- app/tests/test_chain.py
+++ app/tests/test_chain.py
@@ -1,5 +1,4 @@
-from app.chain.steps import PromptBuilder
-from app.chain.steps import PromptBuilderInput
+from app.chain.steps import PromptBuilder,PromptBuilderInput, LLMRunner, PromptBuilderOutput
 
 def test_prompt_builder():
     step = PromptBuilder()
@@ -15,3 +14,20 @@ def test_prompt_builder():
     assert "mean=10 std=2" in output.prompt
     
 
+def test_llm_runner(monkeypatch):
+    def fake_pipeline(*args, **kwargs):
+        return [{"generated_text": "ANSWER: test response"}]
+
+    monkeypatch.setattr("app.chain.steps.generator", fake_pipeline)
+
+    step = LLMRunner()
+
+    input_data = PromptBuilderOutput(
+        prompt="fake prompt",
+        question="test?"
+    )
+
+    output = step.invoke(input_data)
+
+    assert "test response" in output.raw_response
+    assert output.question == "test?"
